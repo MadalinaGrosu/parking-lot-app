9d4c08aa39573f784922b1ae1ac2effcc93ef718
Madalina Grosu <madalina92.grosu@gmail.com>
2018-02-26 22:14:32 +0200

    Add concatenating step

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,11 +5,18 @@ module.exports = function (grunt) {
         },
         jshint: {
             all: ['Gruntfile.js', 'app/js/*.js', 'test/*.js']
+        },
+        concat: {
+            dist: {
+                src: ["app/js/*.js"],
+                dest: "dist/js/scripts.js"
+            }
         }
     });
 
     grunt.loadNpmTasks("grunt-contrib-clean");
     grunt.loadNpmTasks("grunt-contrib-jshint");
+    grunt.loadNpmTasks("grunt-contrib-concat");
 
-    grunt.registerTask("default", ["clean", "jshint"]);
+    grunt.registerTask("default", ["clean", "jshint", "concat"]);
 };
