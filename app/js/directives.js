90a32f03c0dbba6fd326fbae5b0d303ed83c98e6
Madalina Grosu <madalina92.grosu@gmail.com>
2018-06-10 13:53:27 +0300

    Configure directive scope

--- app/js/directives.js
+++ app/js/directives.js
@@ -1,6 +1,10 @@
 parking.directive("alert", function () {
     return {
         restrict: 'E',
+        scope: {
+            topic: '@topic',
+            description: '@description'
+        },
         templateUrl: "alert.html",
         replace: true
     };
