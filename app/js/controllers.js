324ff37d0b22541f4d85f17fe45653e11ec77726
Madalina Grosu <madalina92.grosu@gmail.com>
2018-06-10 13:57:38 +0300

    Use bidirectional binding in alert directive

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,6 +2,8 @@ parking.controller("parkingCtrl", function ($scope) {
     $scope.appTitle = "[Packt] Parking";
     $scope.cars = [];
     $scope.colors = ["White", "Black", "Blue", "Red", "Silver"];
+    $scope.alertTopic = "Something went wrong!";
+    $scope.descriptionTopic = "You must inform the plate and the color of the car!";
     
     $scope.park = function (car) {
         car.entrance = new Date();
