97bb4456580dc12298e4cd815a7fb55817dad394
Madalina Grosu <madalina92.grosu@gmail.com>
2018-06-10 13:44:53 +0300

    Move html snippet to template

--- app/js/directives.js
+++ app/js/directives.js
@@ -1,12 +1,5 @@
 parking.directive("alert", function () {
     return {
-        template: "<div class='alert'>" +
-            "<span class='alert-topic'>" +
-            "Something went wrong!" +
-            "</span>" +
-            "<span class='alert-description'>" +
-            "You must inform the plate and the color of the car!" +
-            "</span>" +
-            "</div>"
+        templateUrl: "alert.html"
     };
 })
